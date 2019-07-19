b768c104b8c1916e603ed9742536d3e2821665e4
SilverwearCode <matous.havranek@gmail.com>
2019-07-19 17:11:43 +0200

    Prvni verze od Mariana

---
+++ HostedServicesWeb/WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace WebApplication1
+{
+    public class RefresherService : IHostedService
+    {
+        private Timer _timer;
+        private HttpClient httpClient;
+
+        public RefresherService()
+        {
+            this.httpClient = new HttpClient();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(DoWork, null, 0, 10000);
+            
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            Console.WriteLine("Test periody!");
+            var x = this.httpClient.GetAsync("http://taskdesk.eu/djqwkldqjwdkl").GetAwaiter().GetResult().Content;
+            var content = x.ReadAsStringAsync().GetAwaiter().GetResult();
+            Console.WriteLine(content);
+
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+    }
+
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHostedService<RefresherService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
